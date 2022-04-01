623c9e23901aea5bd1d87cf04d596f3418a7cf56
Kartik Mishra <kartikm2219@gmail.com>
2022-04-01 18:06:11 +0530

    made some changes and cleaned deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,22 @@
+/* Script to DEPLOY contract using HARDHAT */
+
 const main = async () => {
+  // Connecting to contract and deploying
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const contractBalance = 0.01;
   const waveContract = await waveContractFactory.deploy({
-    value: hre.ethers.utils.parseEther("0.01"),
+    value: hre.ethers.utils.parseEther(contractBalance.toString()),
   });
-  await waveContract.deployed();
+  await waveContract.deployed(); // await deployment
 
-  const [deployer] = await hre.ethers.getSigners();
-  const accountBalance = await deployer.getBalance();
+  const [deployer] = await hre.ethers.getSigners(); // contract owner
+  const accountBalance = await deployer.getBalance(); // owner balance
 
-  console.log("Deploying contracts with account: ", deployer.address);
-  console.log("Account Balance: ", accountBalance.toString());
+  console.log("Deploying Contract with account: ", deployer.address); // owner address
+  console.log("Account Balance: ", accountBalance.toString()); // owner balance
 
-  console.log("WavePortal Address: ", waveContract.address);
+  console.log("Contract Address: ", waveContract.address); // contract address generated
+  console.log("Contract Balance: ", contractBalance, "Ether"); // contract balance
 };
 
 const runMain = async () => {
